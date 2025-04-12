6e2f3e08db379c06d63859fe1b20ff5b38987582
thekidkid <rolextk07@gmail.com>
2025-04-12 23:00:38 +0200

    Add Render deployment configuration and update CORS settings

--- server.js
+++ server.js
@@ -7,7 +7,10 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://ryne.netlify.app', 'http://localhost:3000'],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
